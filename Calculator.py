4091b2a06a4bc04c195b4263bf535e53799f56e4
Rimshanaz05 <rimshanaaz697@gmail.com>
2025-08-13 19:12:51 +0530

    Update Calculator.py
    fixed a bug in the code

--- Calculator.py
+++ Calculator.py
@@ -12,7 +12,7 @@ print("2:Substract")
 print("3:Multiply")
 print("4:Divide")
 while True:
-   choice=float(input("Choose from 1/2/3/4"))
+   choice=(input("Choose from 1/2/3/4= "))
    if choice in('1','2','3','4'):
        try:
            num1=float(input("Enter the first number "))
@@ -25,7 +25,7 @@ while True:
        elif choice =='2':
            print("Result=",sub(num1,num2))
        elif choice =='3':
-           print("REsult=",multiply(num1,num2))
+           print("Result=",multiply(num1,num2))
        elif choice =='4':
            print("Result=",divide(num1,num2))
        else:
